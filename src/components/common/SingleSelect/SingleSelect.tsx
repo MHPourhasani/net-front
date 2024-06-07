cd5ee873f6c92d47b840f0b052e4dd1a6ce85c25
MHPourhasani <mhpourhasani.dev@gmail.com>
2024-06-08 01:03:08 +0330

    update signup page

--- src/components/common/SingleSelect/SingleSelect.tsx
+++ src/components/common/SingleSelect/SingleSelect.tsx
@@ -87,82 +87,85 @@ const SingleSelect = (props: SingleSelectProps) => {
     });
 
     return (
-        <div
-            ref={singleSelectRef}
-            className={cn(
-                `relative w-full hover:border-gray-200 ${props.defaultValue || selectedOption?.name ? "border-gray-200 bg-transparent" : "border-transparent"} ${props.disabled ? "cursor-not-allowed bg-gray-100" : "cursor-pointer"}`,
-                props.className
-            )}
-        >
+        <div className="flex flex-col">
             {props.label && <label>{props.label}</label>}
 
             <div
-                onClick={() => {
-                    if (!props.disabled) setShowOptions(!showOptions);
-                }}
-                className={`mt-2 flex h-12 w-full items-center gap-2 rounded-lg border p-2 ${showOptions ? "rounded-b-none border-b-0 !border-sky-500" : "bg-gray-100"}`}
+                ref={singleSelectRef}
+                className={cn(
+                    `relative w-full hover:border-gray-200 ${props.defaultValue || selectedOption?.name ? "border-gray-200 bg-transparent" : "border-transparent"} ${props.disabled ? "cursor-not-allowed bg-gray-100" : "cursor-pointer"}`,
+                    props.className
+                )}
             >
-                <div className="flex flex-1 items-center gap-4">
-                    {selectedOption?.name && <span className="max-w-[200px] truncate text-sm">{selectedOption?.name}</span>}
-
-                    {!selectedOption?.name && (
-                        <input
-                            dir="auto"
-                            {...props.inputProps}
-                            onClick={(e: any) => {
-                                e.stopPropagation();
-                                setShowOptions(true);
-                            }}
-                            onChange={changeHandler}
-                            onKeyDown={keyDownHandler}
-                            disabled={props.disabled}
-                            className={cn(
-                                `w-full border-0 bg-transparent px-4 text-sm text-gray-900 outline-none placeholder:text-left focus:border-0 focus:outline-none disabled:cursor-not-allowed rtl:placeholder:text-right`,
-                                props.inputProps?.className
-                            )}
-                        />
-                    )}
-                </div>
-
-                <span
-                    onClick={() => {
-                        if (selectedOption?.name && !props.disabled) {
-                            setSelectedOption(null);
-                        } else if (!props.disabled) {
-                            setShowOptions(!showOptions);
-                        }
-                    }}
-                    className={`flex size-6 items-center justify-center rounded-full transition-all ease-in-out hover:bg-gray-200 ${props.disabled ? "cursor-not-allowed" : "cursor-pointer"}`}
-                >
-                    <img
-                        src={selectedOption?.name ? close : arrow}
-                        alt={selectedOption?.name ? "close" : "arrow"}
-                        className={`transition-all ease-in-out ${showOptions && !selectedOption?.name ? "rotate-180" : ""}`}
-                    />
-                </span>
-            </div>
-
-            {showOptions && !props.disabled && (
                 <div
-                    className={`no-scrollbar border-1.5 absolute z-10 flex h-fit max-h-96 w-full flex-col overflow-y-auto rounded-b-lg border-t-0 border-sky-500 bg-white text-gray-900 shadow-md`}
+                    onClick={() => {
+                        if (!props.disabled) setShowOptions(!showOptions);
+                    }}
+                    className={`mt-2 flex h-12 w-full items-center gap-2 rounded-lg border p-2 ${showOptions ? "rounded-b-none border-b-0 !border-sky-500" : "bg-gray-100"}`}
                 >
-                    {filteredOptions.length ? (
-                        filteredOptions.map((option) => (
-                            <div
-                                key={option.name}
-                                onClick={() => selectOptionHandler(option)}
-                                className={`hover:bg-gray-250 w-full cursor-pointer truncate py-3 last-of-type:rounded-b-[10px] ${filteredOptions.findIndex((item) => item === option) === index ? "bg-gray-250" : ""} ${selectedOption?.name === option.name ? "cursor-not-allowed text-gray-200" : ""}`}
-                            >
-                                <span className={`w-full truncate px-4 text-sm`}>{option.name}</span>
-                            </div>
-                        ))
-                    ) : (
-                        <p dir="auto" className="cursor-default px-4 py-3 text-sm">
-                            {props.emptySearchText || "آیتمی وجود ندارد."}
-                        </p>
-                    )}
+                    <div className="flex flex-1 items-center gap-4">
+                        {selectedOption?.name && <span className="max-w-[200px] truncate text-sm">{selectedOption?.name}</span>}
+
+                        {!selectedOption?.name && (
+                            <input
+                                dir="auto"
+                                {...props.inputProps}
+                                onClick={(e: any) => {
+                                    e.stopPropagation();
+                                    setShowOptions(true);
+                                }}
+                                onChange={changeHandler}
+                                onKeyDown={keyDownHandler}
+                                disabled={props.disabled}
+                                className={cn(
+                                    `w-full border-0 bg-transparent px-4 text-sm text-gray-900 outline-none placeholder:text-left focus:border-0 focus:outline-none disabled:cursor-not-allowed rtl:placeholder:text-right`,
+                                    props.inputProps?.className
+                                )}
+                            />
+                        )}
+                    </div>
+
+                    <span
+                        onClick={() => {
+                            if (selectedOption?.name && !props.disabled) {
+                                props.onChange({});
+                                setSelectedOption(null);
+                            } else if (!props.disabled) {
+                                setShowOptions(!showOptions);
+                            }
+                        }}
+                        className={`flex size-6 items-center justify-center rounded-full transition-all ease-in-out hover:bg-gray-200 ${props.disabled ? "cursor-not-allowed" : "cursor-pointer"}`}
+                    >
+                        <img
+                            src={selectedOption?.name ? close : arrow}
+                            alt={selectedOption?.name ? "close" : "arrow"}
+                            className={`transition-all ease-in-out ${showOptions && !selectedOption?.name ? "rotate-180" : ""}`}
+                        />
+                    </span>
                 </div>
-            )}
+
+                {showOptions && !props.disabled && (
+                    <div
+                        className={`no-scrollbar absolute z-10 flex h-fit max-h-96 w-full flex-col overflow-y-auto rounded-b-lg border border-t-0 border-sky-500 bg-white text-gray-900 shadow-md`}
+                    >
+                        {filteredOptions.length ? (
+                            filteredOptions.map((option) => (
+                                <div
+                                    key={option.name}
+                                    onClick={() => selectOptionHandler(option)}
+                                    className={`hover:bg-gray-250 w-full cursor-pointer truncate py-3 last-of-type:rounded-b-[10px] ${filteredOptions.findIndex((item) => item === option) === index ? "bg-gray-250" : ""} ${selectedOption?.name === option.name ? "cursor-not-allowed text-gray-200" : ""}`}
+                                >
+                                    <span className={`w-full truncate px-4 text-sm`}>{option.name}</span>
+                                </div>
+                            ))
+                        ) : (
+                            <p dir="auto" className="cursor-default px-4 py-3 text-sm">
+                                {props.emptySearchText || "آیتمی وجود ندارد."}
+                            </p>
+                        )}
+                    </div>
+                )}
+            </div>
         </div>
     );
 };
--- src/pages/auth/signup.tsx
+++ src/pages/auth/signup.tsx
@@ -10,12 +10,19 @@ import { toastMessage } from "../../utils/toastMessage";
 import { toast } from "react-toastify";
 import Input from "../../components/common/Input/Input";
 import Button from "../../components/common/Button/Button";
-import loginImage from "../../assets/images/svg/login-page.svg";
+import signupImage from "../../assets/images/svg/signup-page.svg";
+import SingleSelect from "../../components/common/SingleSelect/SingleSelect";
 
 const SignupPage = () => {
-    const [formData, setFormData] = useState({ username: "", password: "", confirmPassword: "" });
+    const [formData, setFormData] = useState({ first_name: "", last_name: "", username: "", job: "", password: "", confirmPassword: "" });
     const navigate = useNavigate();
     // const dispatch = useAppDispatch();
+
+    const jobs = [
+        { name: "کارگر", key: "worker" },
+        { name: "اپراتور", key: "operator" },
+        { name: "متخصص", key: "repairman" }
+    ];
 
     const changeHandler = (e: any) => {
         setFormData({ ...formData, [e.target.name]: e.target.value });
@@ -24,7 +31,7 @@ const SignupPage = () => {
     const signupHandler = async () => {
         try {
             if (formData.password !== formData.confirmPassword) {
-                console.log(toastMessage(7));
+                toast.error(toastMessage(7));
             } else {
                 const res = await fetch(API.profile.createPersonnel(), {
                     method: "POST",
@@ -32,12 +39,16 @@ const SignupPage = () => {
                         "Content-Type": "application/json",
                         Accept: "application/json"
                     },
-                    body: JSON.stringify(formData)
+                    body: JSON.stringify({ ...formData, confirmPassword: undefined })
                 });
                 const data = await res.json();
-                authToken.set(data);
-                navigate(PATH.dashboard, { replace: true });
-                toast.success(toastMessage(5));
+                if (res.status >= 400) {
+                    toast.error(toastMessage(17));
+                } else {
+                    authToken.set(data);
+                    navigate(PATH.dashboard, { replace: true });
+                    toast.success(toastMessage(15));
+                }
             }
         } catch (error) {
             console.error(error);
@@ -46,21 +57,46 @@ const SignupPage = () => {
 
     return (
         <section className="flex min-h-screen w-full bg-white md:items-center md:gap-10 xl:gap-0">
-            <div className="md:bg-bg-2 hidden md:flex md:h-screen md:w-full md:flex-1 md:items-center md:justify-center">
-                <img src={loginImage} alt="login image" className="h-auto w-full 2xl:w-9/12" />
+            <div className="hidden md:flex md:h-screen md:w-full md:flex-1 md:items-center md:justify-center md:bg-sky-50">
+                <img src={signupImage} alt="login image" className="h-auto w-full 2xl:w-9/12" />
             </div>
 
             <div className="flex w-full items-center justify-center p-4 md:flex-1 md:p-0">
                 <div className="flex w-full flex-col gap-4 md:w-11/12 lg:w-9/12 xl:w-8/12 2xl:w-6/12 2xl:max-w-[600px]">
                     <h1 className="mb-5 text-3xl font-bold">ثبت نام</h1>
-                    <Input label="نام کاربری" name="username" placeholder="example@gmail.com" value={formData.username} onChange={changeHandler} />
-                    <Input type="password" label="رمز عبور" name="password" value={formData.password} onChange={changeHandler} />
-                    <Input type="password" label="تکرار رمز عبور" name="confirmPassword" value={formData.confirmPassword} onChange={changeHandler} />
+
+                    <Input label="نام" name="first_name" value={formData.first_name} onChange={changeHandler} />
+                    <Input label="نام خانوادگی" name="last_name" value={formData.last_name} onChange={changeHandler} />
+                    <Input dir="ltr" label="نام کاربری" name="username" value={formData.username} onChange={changeHandler} />
+                    <SingleSelect
+                        label="شغل"
+                        options={jobs}
+                        onChange={(selected) => setFormData({ ...formData, job: selected.key })}
+                        emptySearchText="شغلی یافت نشد."
+                    />
+                    <Input dir="ltr" type="password" label="رمز عبور" name="password" value={formData.password} onChange={changeHandler} />
+                    <Input
+                        dir="ltr"
+                        type="password"
+                        label="تکرار رمز عبور"
+                        name="confirmPassword"
+                        value={formData.confirmPassword}
+                        onChange={changeHandler}
+                    />
 
                     <Button
                         variant="Primary"
                         onClick={signupHandler}
-                        disabled={!(formData.username && formData.password && formData.confirmPassword)}
+                        disabled={
+                            !(
+                                formData.first_name &&
+                                formData.last_name &&
+                                formData.username &&
+                                formData.job &&
+                                formData.password &&
+                                formData.confirmPassword
+                            )
+                        }
                         className="disabled:bg-gray-300"
                     >
                         ثبت نام
