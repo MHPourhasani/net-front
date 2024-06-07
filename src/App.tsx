facdc7ba4a7b981408c111963194bd418c3c6706
MHPourhasani <mhpourhasani.dev@gmail.com>
2024-06-08 01:04:46 +0330

    fix some bugs

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes, useNavigate } from "react-router-dom";
 import { PATH } from "./utils/path";
 import LoginPage from "./pages/auth/login";
 import { authToken } from "./utils/storage";
@@ -7,40 +7,45 @@ import Layout from "./components/Layout/Layout";
 import SignupPage from "./pages/auth/signup";
 import { ToastContainer } from "react-toastify";
 import PrivateRoute from "./components/PrivateRoute/PrivateRoute";
+import { useEffect } from "react";
 
 function App() {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        navigate(PATH.dashboard);
+    }, []);
+
     return (
         <main className="flex h-screen w-full items-center justify-center overflow-hidden bg-gray-100">
-            <ToastContainer position="bottom-left" autoClose={3000} theme="dark" />
+            <ToastContainer rtl position="bottom-left" autoClose={3000} theme="dark" />
 
-            <BrowserRouter>
-                <Routes>
-                    <>
-                        <Route path={PATH.login} element={authToken.get()?.access ? <Navigate to={PATH.dashboard} replace /> : <LoginPage />} />
-                        <Route path={PATH.signup} element={authToken.get()?.access ? <Navigate to={PATH.dashboard} replace /> : <SignupPage />} />
-                    </>
+            <Routes>
+                <>
+                    <Route path={PATH.login} element={authToken.get()?.access ? <Navigate to={PATH.dashboard} replace /> : <LoginPage />} />
+                    <Route path={PATH.signup} element={authToken.get()?.access ? <Navigate to={PATH.dashboard} replace /> : <SignupPage />} />
+                </>
 
-                    <Route
-                        element={
-                            <PrivateRoute>
-                                <Layout />
-                            </PrivateRoute>
-                        }
-                    >
-                        {routes.map((route) => (
-                            <Route
-                                key={route.name}
-                                {...route}
-                                element={
-                                    <PrivateRoute>
-                                        <route.component />
-                                    </PrivateRoute>
-                                }
-                            />
-                        ))}
-                    </Route>
-                </Routes>
-            </BrowserRouter>
+                <Route
+                    element={
+                        <PrivateRoute>
+                            <Layout />
+                        </PrivateRoute>
+                    }
+                >
+                    {routes.map((route) => (
+                        <Route
+                            key={route.name}
+                            {...route}
+                            element={
+                                <PrivateRoute>
+                                    <route.component />
+                                </PrivateRoute>
+                            }
+                        />
+                    ))}
+                </Route>
+            </Routes>
         </main>
     );
 }
